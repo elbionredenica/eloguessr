4e7f09c256c0ee322edd3d4a0139621203894228
Elbion Redenica <elbion@uni.minerva.edu>
2024-12-31 17:40:37 -0300

    deployment

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Navigate to your project directory
-cd /root/eloguessr
+cd /root/rating-guessr
 
 # Stop and remove existing containers and volumes
 docker compose down -v  # Use -v instead of --volumes
